f32784a2df07d71a24b87e07ac03acd7de1f665d
程序员阿胜 <2173835178@qq.com>
2022-12-13 21:43:17 +0800

    sdvds

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -3,7 +3,7 @@ layui.use(['form','jquery','jquery_cookie'], function () {
         layer = layui.layer,
         $ = layui.jquery,
         $ = layui.jquery_cookie($);
-    
+
     // 表单submit提交
     form.on('submit(login)',function (data){
         console.log(data.field) //当前容器的全部表单字段,明值对形式:{name:value}
@@ -37,6 +37,54 @@ layui.use(['form','jquery','jquery_cookie'], function () {
                 }
             }
         })
-        return false; //阻止表单跳转,如果需要表单跳转,去掉这段即可.
+        return false; //阻止表单跳转
     })
 });
+var flag = true;
+var start = document.getElementById("first_head");
+start.onclick = function(){
+    if (flag) {
+        start.style.color = "red";
+            document.getElementById("one").innerText = "Y";
+            document.getElementById("two").innerText = "J";
+            document.getElementById("three").innerText = "X";
+            document.getElementById("four").innerText = "六";
+            document.getElementById("five").innerText = "组";
+            document.getElementById("sex").innerText = "项";
+            document.getElementById("seven").innerText = "目";
+        flag = false;
+    } else {
+        document.getElementById("one").innerText = "E";
+        document.getElementById("two").innerText = "R";
+        document.getElementById("three").innerText = "P";
+        document.getElementById("four").innerText = "后";
+        document.getElementById("five").innerText = "端";
+        document.getElementById("sex").innerText = "登";
+        document.getElementById("seven").innerText = "录";
+        flag = true;
+    }
+}
+var open = true;
+var back = document.getElementById("back");
+var border = document.getElementById("border");
+document.getElementById("light").onclick = function (){
+    if (open) {
+        back.style.width = "100%";
+        back.style.height = "100%";
+        back.style.overflow = "hidden";
+        back.style.background = "white";
+        back.style.transition = "all 3s";
+        document.getElementById("span_light_one").innerText = "黑";
+        document.getElementById("span_light_two").innerText = "夜";
+        open = false
+    } else {
+        back.style.width = "100%";
+        back.style.height = "100%";
+        back.style.overflow = "hidden";
+        back.style.background = "black";
+        back.style.transition = "all 3s";
+        document.getElementById("span_light_one").innerText = "白";
+        document.getElementById("span_light_two").innerText = "天";
+        open = true
+    }
+}
